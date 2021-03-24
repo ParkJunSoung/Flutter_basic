def69ebac9b8ec52d5ec39435ada2bc22ffc6191
JunSeoung <madox9999@gmail.com>
2021-03-24 11:40:42 +0900

    성적계산프로그램2과학추가

--- lib/3_24.dart
+++ lib/3_24.dart
@@ -2,18 +2,19 @@ void main() {
   int kor = 100;
   int math = 50;
   int english = 70;
+  int science = 30;
 
-  int sum = kor + math + english);
-  double average = sum / 3;
+  int sum = total(kor, math, english);
+  double average = avg(kor, math, english);
 
   print('합계 : $sum');
   print('평균 : ${average.toStringAsFixed(2)}');
 }
 
-void total(int korean, int math, int english) {
-  return korean + math + english;
+void total(int korean, int math, int english, int science) {
+  return korean + math + english + science;
 }
 
-double avg(int korean, int math, int english) {
-  return (korean + math + english) / 3;
+double avg(int korean, int math, int english, int science) {
+  return (korean + math + english + science) / 4;
 }
