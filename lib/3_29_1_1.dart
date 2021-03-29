b94427f50cd12edd41f4a8a827d6b3be52d20f01
준성 <madox9999@gmail.com>
2021-03-29 15:22:59 +0900

    퀴즈1

--- lib/3_29_1_1.dart
+++ lib/3_29_1_1.dart
@@ -2,15 +2,25 @@ void main() {
   Word word = Word('abcdefgherjklhnm');
   print(word.isVowel(0)); // true
   print(word.isVowel(1));
-  print(word.isVowel(2));
-  print(word.isVowel(3));
-  print(word.isVowel(4));
-  print(word.isVowel(5));
-  print(word.isVowel(6));
-  print(word.isVowel(7));
-  print(word.isVowel(8));
-  print(word.isVowel(9));
-  print(word.isVowel(10));
+  Word dish = Word('dish');
+  print(dish.toPlural()); // dishes
+  Word cherry = Word('cherry');
+  print(cherry.toPlural()); // cherries
+  Word clock = Word('clock');
+  print(clock.toPlural()); // clocks
+  Word tax = Word('tax');
+  print(tax.toPlural()); // taxes
+  Word bench = Word('bench');
+  print(bench.toPlural()); // benches
+  Word tomato = Word('tomato');
+  print(tomato.toPlural()); // tomatoes
+  Word baby = Word('baby');
+  print(baby.toPlural()); // babies
+  Word leaf = Word('leaf');
+  print(leaf.toPlural()); // leaves
+  Word live = Word('live');
+  print(live.toPlural()); // lives
+
 }
 
 class Word {
@@ -19,7 +29,30 @@ class Word {
   Word(this.letters);
 
   // i번째 글자가 모음이면 true  // a, i, u, e, o
-  bool isVowel(int i) => 'aiueo'.contains(letters.substring(i, i + 1).toLowerCase());
+  bool isVowel(int i) =>
+      'aiueo'.contains(letters.substring(i, i + 1).toLowerCase());
 
+  // i번째 글자가 자음이면
   bool isConsonant(int i) => !isVowel(i);
-}
+
+  //복수형으로 변환
+  String toPlural() {
+    if (letters.endsWith('s') || letters.endsWith('x') ||
+        letters.endsWith('ch') || letters.endsWith('sh') ||
+        letters.endsWith('o')) {
+      return letters + 'es';
+    }
+    if (letters.endsWith('f') || letters.endsWith('fe')) {
+      return letters.replaceAll('f', 'ves');
+    }
+    else if (letters.endsWith('a') || letters.endsWith('i') || letters.endsWith('u') ||
+        letters.endsWith('e') || letters.endsWith('o')) {
+      return letters.replaceAll('y', 'ies');
+    } else
+      return letters + 's';
+
+    }
+
+  }
+
+
