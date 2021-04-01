2b696a1585db123be2f494e047b4e892ce3c49eb
준성 <madox9999@gmail.com>
2021-04-01 11:14:57 +0900

    숫자비교하기

---
+++ lib/4_01/04_01_1.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+import 'dart:math' as math;
+void main() {
+  // 입력 1 ~ 99
+  Exam exam = Exam();
+  for (int i = 0; i < 5; i++) {
+    int input = int.parse(stdin.readLineSync());
+    exam.numbers.add(input);
+  }
+  print(exam.max());
+  print(exam.min());
+}
+class Exam {
+  List<int> numbers = [];
+  int max() {
+    int result = 0;
+    for (int i = 0; i < numbers.length; i++) {
+      // if (result < numbers[i]) {
+      //   result = numbers[i];
+      // }
+      result = math.max(result, numbers[i]);
+    }
+    return result;
+  }
+  int min() {
+    int result = 99;
+    for (int i = 0; i < numbers.length; i++) {
+      // if (result > numbers[i]) {
+      //   result = numbers[i];
+      // }
+      result = math.min(result, numbers[i]);
+    }
+    return result;
+  }
+}
