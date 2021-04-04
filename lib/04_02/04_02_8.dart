1844929c8566556b6871bb7914641b8ecfbfe385
준성 <madox9999@gmail.com>
2021-04-04 16:31:21 +0900

    알고리즘문제8번_복습

--- lib/04_02/04_02_8.dart
+++ lib/04_02/04_02_8.dart
@@ -1,13 +1,20 @@
 import 'dart:io';
 void main() {
-  //입력받기
-  String E = stdin.readLineSync();
-  // ten은 E에들어있는 랭스값중에 < 문자가 표시된건
-  //<///////+<<</+////
-  int ten = E.length - E.replaceAll('<', '').length;
-  int one = E.length - E.replaceAll('/', '').length;
+  // //입력받기
+  // String E = stdin.readLineSync();
+  // // ten은 E에들어있는 랭스값중에 < 문자가 표시된건
+  // //<///////+<<</+////
+  // //6 - 4 = 2
+  // int ten = E.length - E.replaceAll('<', '').length;
+  // int one = E.length - E.replaceAll('/', '').length;
+  //
+  // print(ten * 10 + one);
+  //
+  List<String> E = stdin.readLineSync().split('+');
 
-  print(ten * 10 + one);
+  for(int i =0; i < E.length; i++)  {
+    if (E[i] = '<') {
 
-
+    }
+  }
 }
